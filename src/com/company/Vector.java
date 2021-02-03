0f805723ca868a976dd74c036e9485fea5d3beba
SIGON12 <nik_b_2020@mail.ru>
2021-02-03 12:31:48 +0300

    hz commit

--- src/com/company/Vector.java
+++ src/com/company/Vector.java
@@ -7,10 +7,9 @@ public class Vector implements Arifmetic{
 
     public void CreateVector() {
         int x = sc.nextInt();
-        int[] arrv = new int[x];
-        {
+        int[] arrv = new int[x];{
             for (int i = 0; i < arrv.length; i++) {
-                System.out.println("Вектор = " + arrv);
+               arrv[i] = 1;}{  System.out.println("Вектор = " + arrv);
             }
 
         }
@@ -23,7 +22,6 @@ public class Vector implements Arifmetic{
 
     @Override
     public void sravn() {
-
 
 
     }
